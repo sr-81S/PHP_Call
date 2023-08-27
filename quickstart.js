593a8380db22b36efda85358a0ad5a3fa81fd7cd
saroz savar <sarojsabar12345@gmail.com>
2023-08-27 16:20:13 +0530

    change

--- quickstart.js
+++ quickstart.js
@@ -41,7 +41,7 @@
 
       Twilio.Device.incoming(function (conn) {
         log('Incoming connection from ' + conn.parameters.From);
-        var archEnemyPhoneNumber = '+12099517118';
+        var archEnemyPhoneNumber = '+19474652764';
 
         if (conn.parameters.From === archEnemyPhoneNumber) {
           conn.reject();
