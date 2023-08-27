993f22d878dd440c2233030771c78c5d10459176
saroz savar <sarojsabar12345@gmail.com>
2023-08-27 16:16:21 +0530

    change

--- request.php
+++ request.php
@@ -2,5 +2,5 @@
 header('Content-type: text/xml');
 ?>
 <Response>
-    <Dial callerId="+12029526015"><?php  echo $_POST['To'];?></Dial>
+    <Dial callerId="+19474652764"><?php  echo $_POST['To'];?></Dial>
 </Response>
